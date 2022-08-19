bbbcaeb079087e3580ea7365db69d746684b981e
Luh <60015267+Luh-code@users.noreply.github.com>
2022-08-19 23:35:02 +0200

    linux support, better colors, small adjustments
    For use in Linux add 'LHG_LINUX' to the compile definitions

--- logger.h
+++ logger.h
@@ -1,19 +1,22 @@
+#pragma once
 /* date = June 23rd 2022 11:37 am */
 
 /*
 Heavy Mod of Pilzschaf's (https://www.github.com/Pilzschaf) simple_logger.
 Major changes:
  - support for console attributes
- - new log levels: debug, critical
+- new log levels: debug, critical
 */
 
-#ifndef LOGGER_H
-#define LOGGER_H
+/*#ifndef LOGGER_H
+#define LOGGER_H*/
 
 #include <iostream>
 #include <string>
-#include <windows.h>
+//#include <windows.h>
+#ifndef LHG_LINUX
 #include <wincon.h>
+#endif // LHG_LINUX
 #include <sstream>
 
 #define LOG_INFO(...) lhg::log_info(__FILE__, __LINE__, false, __VA_ARGS__)
@@ -25,10 +28,10 @@ Major changes:
 #define LOG_EMPTY() lhg::log_empty()
 
 namespace lhg
-{   
+{
     struct Col
     {
-        public: 
+        public:
         const char* att;
         const int col;
         
@@ -36,14 +39,25 @@ namespace lhg
             : col(col), att(att){};
     };
     
-    static Col normal_col = Col("\033[3;40;37m", FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-    static Col info_col = Col("\033[3;40;37m", FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-    static Col debug_col = Col("\033[3;40;96m", FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-    static Col warning_col = Col("\033[3;40;33m", FOREGROUND_RED | FOREGROUND_INTENSITY);
-    static Col error_col = Col("\033[3;40;31m", FOREGROUND_RED);
+    enum WinConCol : int32_t
+    {
+        FOREGROUND_BLUE = 0x0001,
+        FOREGROUND_GREEN = 0x0002,
+        FOREGROUND_RED = 0x0004,
+        FOREGROUND_INTENSITY = 0x0008,
+        BACKGROUND_BLUE = 0x0010,
+        BACKGROUND_GREEN = 0x0020,
+        BACKGROUND_RED = 0x00040,
+        BACKGROUND_INTENSITY = 0x00080,
+    };
+
+    static Col normal_col = Col("\033[3;00;37m", FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+    static Col info_col = Col("\033[3;00;37m", FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    static Col debug_col = Col("\033[3;00;96m", FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    static Col warning_col = Col("\033[3;00;33m", FOREGROUND_RED | FOREGROUND_INTENSITY);
+    static Col error_col = Col("\033[3;00;31m", FOREGROUND_RED);
     static Col critical_col = Col("\033[3;101;37m", /*FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | /*FOREGROUND_INTENSITY |*/ BACKGROUND_RED /*| BACKGROUND_INTENSITY*/);
     
-    static bool use_attributes = false;
 #ifdef LHG_VERBOSE
     static bool verbose = true;
 #else
@@ -51,7 +65,12 @@ namespace lhg
 #endif // LHG_VERBOSE
     static bool show_path = true;
     
+#ifndef LHG_LINUX
+    static bool use_attributes = false;
     static HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+#else
+    static const bool use_attributes = true;
+#endif
     
     static void variadic_unpack(std::string& buf) {}
     
@@ -166,9 +185,13 @@ namespace lhg
         {
             col_att = (char*) "";
             norm_att = (char*) "";
+#ifndef LHG_LINUX
             SetConsoleTextAttribute(hConsole, col.col);
+#endif // LHG_LINUX
             log_line(file, line, msg, col_att, norm_att);
+#ifndef LHG_LINUX
             SetConsoleTextAttribute(hConsole, normal_col.col);
+#endif // LHG_LINUX
             return;
         }
         log_line(file, line, msg, col_att, norm_att);
@@ -221,4 +244,4 @@ namespace lhg
     }
 }
 
-#endif //LOGGER_H
+//#endif //LOGGER_H
